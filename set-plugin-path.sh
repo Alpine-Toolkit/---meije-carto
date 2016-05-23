fa14a52c2decbec6be04e49311258672fdcafa5d
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2016-05-23 12:45:08 +0200

    set-plugin-path.sh

--- set-plugin-path.sh
+++ set-plugin-path.sh
@@ -1,1 +1,19 @@
+####################################################################################################
+#
+# $QT/qml/QtCarto/
+#  qmldir
+#  libqmlqtcartoplugin.so
+
+cat >> imports/QtCarto/qmldir <<EOF
+module QtCarto
+plugin qmlqtcartoplugin
+classname QtCartoDeclarativeModule
+EOF
+
 export QML2_IMPORT_PATH=$PWD/imports
+
+####################################################################################################
+#
+# End
+#
+####################################################################################################
