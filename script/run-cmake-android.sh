97f132eb7bc4b1113ee8b8812a234768cdf018a3
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2016-06-22 21:51:10 +0200

    run-cmake-android

--- script/run-cmake-android.sh
+++ script/run-cmake-android.sh
@@ -2,14 +2,17 @@
 
 # -DANDROID_ABI="armeabi-v7a with NEON"
 
+#BUILD_TYPE=Release
+BUILD_TYPE=Debug
+
 cmake \
   -DCMAKE_TOOLCHAIN_FILE=android.toolchain.cmake \
   -DANDROID_NDK=/opt/android-ndk-r10e \
-  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
   -DANDROID_ABI="armeabi-v7a" \
   $HOME/home/developpement/qtcarto-application/alpine-toolkit/qtcarto
 
-cmake --build
+cmake --build .
 
 ####################################################################################################
 #
